2a9b654b016f0838a4c1b265e70e83c99cc536c0
Guoqing Geng <xgugeng@gmail.com>
2014-02-16 10:22:29 +0800

    重构

---
+++ source/tconnd/tconnd_epoll.c
@@ -0,0 +1,96 @@
+#include "tconnd/tconnd_epoll.h"
+#include "tlibc/core/tlibc_list.h"
+#include "tcommon/terrno.h"
+#include "tconnd/tdtp_instance.h"
+#include "tconnd/tdtp_socket.h"
+
+#include <assert.h>
+#include <errno.h>
+
+#define TDTP_MAX_EVENTS 1024
+TERROR_CODE process_epool(tdtp_instance_t *self)
+{
+	int i;
+	TERROR_CODE ret = E_TS_NOERROR;
+	TLIBC_LIST_HEAD *iter, *next;
+
+	if(tlibc_list_empty(&self->readable_list))
+	{
+		struct epoll_event 	events[TDTP_MAX_EVENTS];
+		int                 events_num;
+		
+		events_num = epoll_wait(self->epollfd, events, TDTP_MAX_EVENTS, 0);
+	    if(events_num == -1)
+		{
+		    if(errno == EINTR)
+		    {
+		        ret = E_TS_WOULD_BLOCK;
+		    }
+		    else
+		    {
+		        ret = E_TS_ERRNO;
+		    }
+			goto done;
+	    }
+
+	    for(i = 0; i < events_num; ++i)
+	    {
+            tdtp_socket_t *socket = events[i].data.ptr;
+            if(socket->readable)
+            {
+                assert(0);
+                continue;
+            }
+            socket->readable = TRUE;
+            tlibc_list_init(&socket->readable_list);
+            tlibc_list_add_tail(&socket->readable_list, &self->readable_list);
+	    }
+	}
+
+	if(tlibc_list_empty(&self->readable_list))
+	{
+        ret = E_TS_WOULD_BLOCK;
+	    goto done;
+	}
+	
+    for(iter = self->readable_list.next; iter != &self->readable_list; iter = next)
+    {
+        TERROR_CODE r;
+        tdtp_socket_t *socket = TLIBC_CONTAINER_OF(iter, tdtp_socket_t, readable_list);
+        next = iter->next;
+        
+        r = tdtp_socket_recv(socket);
+        if(r == E_TS_ERRNO)
+        {
+            switch(errno)
+            {
+                case EAGAIN:
+                    socket->readable = FALSE;
+                    tlibc_list_del(iter);
+                    break;
+                case EINTR:
+                    break;
+                default:
+                    assert(0);
+                    ret = E_TS_ERROR;
+                    goto done;
+            }
+        }
+        else if(r == E_TS_WOULD_BLOCK)
+        {
+            ret = E_TS_WOULD_BLOCK;
+            break;
+        }        
+        else if(r != E_TS_NOERROR)
+        {
+            socket->readable = FALSE;
+            tlibc_list_del(iter);
+            tdtp_socket_free(socket);
+        }
+    }
+
+done:	
+	return ret;
+}
+
+
---
+++ source/tconnd/tconnd_epoll.h
@@ -0,0 +1,9 @@
+#ifndef _H_TCONND_EPOLL_H
+#define _H_TCONND_EPOLL_H
+
+#include "tconnd/tdtp_instance.h"
+
+TERROR_CODE process_epool(tdtp_instance_t *self);
+
+#endif//_H_TCONND_EPOLL_H
+
