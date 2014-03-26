3dfb6bd3b609d10e67e4edc3eb200c2a33adcabe
Guoqing Geng <xgugeng@gmail.com>
2014-03-26 14:58:37 +0800

    优化makefile

--- build_common.mk
+++ build_common.mk
@@ -1,6 +1,7 @@
 #parameter list
 NAME?=
 EXECUTABLE?=0
+USE_CXX?=0
 
 DEPTARET_DIR?=..
 DEPTARGET?=
@@ -11,25 +12,23 @@ DEPLOCALLIBS?=tlibc
 
 PREFIX?=/usr/local/$(NAME)
 
+CINC=-I include 
+CINC+=$(patsubst %, -I $(DEPTARET_DIR)/%/include, $(DEPTARGET))
+CINC+=$(patsubst %, -I %, $(DEPLOCALINCLUDE))
 ifdef MAKE_RELEASE
-CFLAGS?= -Wall -Wconversion -Wcast-qual -Wpointer-arith -Wredundant-decls -Wmissing-declarations -Werror --pipe -O3 $(CINC) -DMAKE_RELEASE
+CFLAGS ?= -Wall -Wconversion -Wcast-qual -Wpointer-arith -Wredundant-decls -Wmissing-declarations -Werror --pipe -O3 $(CINC) -DMAKE_RELEASE
+CXXFLAGS ?= -Wall -Wconversion -Wcast-qual -Wpointer-arith -Wredundant-decls -Wmissing-declarations -Werror --pipe -O3 $(CINC) -DMAKE_RELEASE
 else
-CFLAGS?= -Wall -Wconversion -Wcast-qual -Wpointer-arith -Wredundant-decls -Wmissing-declarations -Werror --pipe -g -ggdb $(CINC) -DMAKE_DEBUG
+CFLAGS ?= -Wall -Wconversion -Wcast-qual -Wpointer-arith -Wredundant-decls -Wmissing-declarations -Werror --pipe -g -ggdb $(CINC) -DMAKE_DEBUG
+CXXFLAGS ?= -Wall -Wconversion -Wcast-qual -Wpointer-arith -Wredundant-decls -Wmissing-declarations -Werror --pipe -g -ggdb $(CINC) -DMAKE_DEBUG
 endif
 #parameter list
 
-CINC=-I include 
-
-
-CINC+=$(patsubst %, -I $(DEPTARET_DIR)/%/include, $(DEPTARGET))
 LDPATH+=$(patsubst %, -L $(DEPTARET_DIR)/%/lib, $(DEPTARGET))
 DEPLIBS+=$(patsubst %, -l %, $(DEPTARGET))
 
-CINC+=$(patsubst %, -I %, $(DEPLOCALINCLUDE))
 LDPATH+=$(patsubst %, -L %, $(DEPLOCALLD))
 DEPLIBS+=$(patsubst %, -l %, $(DEPLOCALLIBS))
-
-
 
 SOURCE=source
 INCLUDE=include
@@ -37,8 +36,10 @@ BINARY=bin
 LIBRARY=lib
 ETC=etc
 TDATA=tdata
+THRIFT=thrift
 
 CC = gcc
+CXX= g++
 AR = ar
 RM = /bin/rm -f
 INSTALL=cp -rpf
@@ -47,10 +48,33 @@ INSTALL=cp -rpf
 
 
 REALCC=$(CC) $(CFLAGS)
+REALCXX=$(CXX) $(CXXFLAGS)
+ifeq ($(USE_CXX),1)
+REALLD=$(CXX) $(LDPATH)
+else
 REALLD=$(CC) $(LDPATH)
+endif
 REALAR=$(AR)
 REALINSTALL=$(INSTALL)
 REALTDATA=$(TDATA) $(CINC)
+REALTHRIFT=$(THRIFT) --gen cpp -out $(INCLUDE)
+
+THRIFT_FILES=$(wildcard $(INCLUDE)/*.thrift)
+THRIFT_SERVICES_FILES=$(wildcard $(INCLUDE)/*_services.thrift)
+
+THRIFT_CONSTANTS_CCFILE=$(THRIFT_FILES:.thrift=_constants.cpp)
+THRIFT_TYPES_CCFILE=$(THRIFT_FILES:.thrift=_types.cpp)
+THRIFT_SERVICES_CCFILE=$(THRIFT_SERVICES_FILES:.thrift=.cpp)
+
+THRIFT_CONSTANTS_HFILE=$(THRIFT_FILES:.thrift=_constants.h)
+THRIFT_TYPES_HFILE=$(THRIFT_FILES:.thrift=_types.h)
+THRIFT_SERVICES_HFILE=$(THRIFT_SERVICES_FILES:.thrift=.h)
+THRIFT_SERVICES_SKELETON_CCFILE=$(THRIFT_SERVICES_FILES:.thrift=_async_server.skeleton.cpp)
+THRIFT_SERVICES_SKELETON_CCFILE+=$(THRIFT_SERVICES_FILES:.thrift=_server.skeleton.cpp)
+
+THRIFT_OFILE=$(THRIFT_CONSTANTS_CCFILE:.cpp=.o)
+THRIFT_OFILE+=$(THRIFT_TYPES_CCFILE:.cpp=.o)
+THRIFT_OFILE+=$(THRIFT_SERVICES_CCFILE:.cpp=.o)
 
 TDATA_FILE=$(wildcard $(INCLUDE)/*.td)
 
@@ -63,7 +87,10 @@ WRITER_CFILE=$(patsubst $(INCLUDE)/%.td, $(SOURCE)/%_writer.c, $(TDATA_FILE))
 WRITER_OFILE=$(WRITER_CFILE:.c=.o)
 
 CFILE=$(wildcard $(SOURCE)/*.c)
+CXXFILE=$(wildcard $(SOURCE)/*.cpp)
+
 OFILE=$(CFILE:.c=.o)
+OFILE+=$(CXXFILE:.cpp=.o)
 
 LIB=$(LIBRARY)/lib$(NAME).a
 APP=$(BINARY)/$(NAME)
@@ -77,19 +104,24 @@ endif
 
 all:$(TYPES_HFILE) $(READER_HFILE) $(WRITER_HFILE) $(TARGET)
 
-$(LIB): $(OFILE) $(WRITER_OFILE) $(READER_OFILE)
+$(LIB): $(OFILE) $(WRITER_OFILE) $(READER_OFILE) $(THRIFT_OFILE)
 	@mkdir -p $(LIBRARY)
 	$(REALAR) r $(LIB) $^
 
-$(APP): $(OFILE) $(WRITER_OFILE) $(READER_OFILE)
+$(APP): $(OFILE) $(WRITER_OFILE) $(READER_OFILE) $(THRIFT_OFILE)
 	@mkdir -p $(BINARY)
 	$(REALLD) -o $@ $^ $(DEPLIBS)
 
 release:
 	$(MAKE) all MAKE_RELEASE=1
 
+ifeq ($(USE_CXX),1)
+%.o: %.cpp
+	$(REALCXX) -o $@ -c $<
+else
 %.o: %.c
 	$(REALCC) -o $@ -c $<
+endif
 
 $(TYPES_HFILE):$(TDATA_FILE)
 	$(REALTDATA) -o $(INCLUDE) -gen types_h $^
@@ -106,6 +138,14 @@ $(READER_CFILE):$(TDATA_FILE)
 $(WRITER_CFILE):$(TDATA_FILE)
 	$(REALTDATA) -o $(SOURCE) -gen writer_c $^
 
+%_constants.cpp:%.thrift
+	$(REALTHRIFT) $<
+
+%_types.cpp:%.thrift
+	$(REALTHRIFT) $<
+
+%_services.cpp:%_services.thrift
+	$(REALTHRIFT) $<
 
 install:
 	@mkdir -p $(PREFIX)
@@ -125,4 +165,9 @@ tags:
 clean:
 	@find . -name "*.o" | xargs $(RM)
 	@$(RM) $(TARGET) $(TYPES_HFILE) $(READER_HFILE) $(WRITER_HFILE) $(READER_CFILE) $(READER_OFILE) $(WRITER_CFILE) $(WRITER_OFILE) tags cscope.in.out cscope.po.out cscope.out
+	@$(RM) $(THRIFT_CONSTANTS_CCFILE) $(THRIFT_TYPES_CCFILE) $(THRIFT_SERVICES_CCFILE)
+	@$(RM) $(THRIFT_CONSTANTS_HFILE) $(THRIFT_TYPES_HFILE) $(THRIFT_SERVICES_HFILE)
+	@$(RM) $(THRIFT_SERVICES_CCFILE)
+	@$(RM) $(THRIFT_SERVICES_HFILE)
+	@$(RM) $(THRIFT_SERVICES_SKELETON_CCFILE)
 
