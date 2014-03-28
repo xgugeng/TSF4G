583d29a9175bba69f81aaf83acc26974ef56ca3d
Guoqing Geng <xgugeng@gmail.com>
2014-03-29 00:53:15 +0800

    拆分tlog中的rolling_file未单独的文件

--- tlog/include/tlog.h
+++ tlog/include/tlog.h
@@ -3,6 +3,7 @@
 
 #include "platform/tlibc_platform.h"
 #include "tlog_config_types.h"
+#include "tlog_rolling_file.h"
 
 #include <stdio.h>
 #include <stdbool.h>
@@ -10,33 +11,23 @@
 
 #define TLOG_VERSION "0.0.1"
 
-
-typedef struct _tlog_rolling_file_instance_t
-{
-	FILE *fout;
-	uint32_t index;
-}tlog_rolling_file_instance_t;
-
-typedef struct _tlog_appender_instance_t
+typedef struct tlog_appender_instance_s
 {
 	tlog_appender_type_t type;
 	tlog_rolling_file_instance_t rolling_file;	
 }tlog_appender_instance_t;
 
-typedef struct _tlog_instance_t
+typedef struct tlog_instance_s
 {
 	uint32_t appender_instance_num;
 	tlog_appender_instance_t appender_instance[TLOG_MAX_APPENDER_NUM];
 }tlog_instance_t;
 
-
-
-typedef struct _tlog_t tlog_t;
-struct _tlog_t
+typedef struct tlog_s
 {
 	tlog_config_t config;
 	tlog_instance_t instance;
-};
+}tlog_t;
 
 
 TERROR_CODE tlog_init(tlog_t *self, const char *config_file);
--- tlog/source/tlog.c
+++ tlog/source/tlog.c
@@ -3,6 +3,7 @@
 
 #include "protocol/tlibc_xml_reader.h"
 #include "tlog_config_reader.h"
+#include "tlog_rolling_file.h"
 
 #include <stdio.h>
 #include <stdarg.h>
@@ -11,95 +12,6 @@
 #include <errno.h>
 #include <unistd.h>
 
-
-static void rolling_file_init(tlog_rolling_file_instance_t *self, const tlog_rolling_file_t *config)
-{
-	uint32_t i;	
-	char file_name[TSERVER_FILE_NAME_LENGH];
-	size_t file_name_len = strlen(config->file_name);
-	strncpy(file_name, config->file_name, file_name_len);
-	file_name[TSERVER_FILE_NAME_LENGH - 1] = 0;
-	for(i = 0; i < file_name_len; ++i)
-	{
-		if(file_name[i] == '/')
-		{
-			file_name[i] = 0;
-			mkdir(file_name, 0755);
-			file_name[i] = '/';
-		}
-	}	
-
-	self->fout = NULL;
-	self->index = 0;
-}
-
-static void rolling_file_log(tlog_rolling_file_instance_t *self, 
-		const tlog_rolling_file_t *config,
-		const char *message, size_t message_size)
-{
-	size_t file_size;
-	long ft;
-
-	if(self->fout == NULL)
-	{
-		self->fout = fopen(config->file_name, "wb+");
-		if(self->fout == NULL)
-		{
-			goto done;
-		}
-		fseek(self->fout, 0, SEEK_END);		
-	}
-	
-	ft = ftell(self->fout);
-	if(ft < 0)
-	{
-	    goto done;
-	}
-	else
-	{
-	    file_size = (size_t)ft;
-	}
-	
-	if(file_size + message_size > config->max_file_size)
-	{
-		char file_name[TSERVER_FILE_NAME_LENGH];
-		snprintf(file_name, TSERVER_FILE_NAME_LENGH, "%s.%u", config->file_name, self->index);
-		++self->index;
-		if(self->index > config->max_backup_index)
-		{
-			self->index = 0;
-		}
-		
-		fclose(self->fout);
-		self->fout = NULL;
-		
-		rename(config->file_name, file_name);
-		
-		self->fout = fopen(config->file_name, "wb+");
-		if(self->fout == NULL)
-		{
-			goto done;
-		}
-		fseek(self->fout, 0, SEEK_END);		
-	}
-	
-	fwrite(message, 1, message_size, self->fout);
-	fputc('\n', self->fout);
-	fflush(self->fout);
-	
-done:
-	return;
-}
-
-static void rolling_file_fini(tlog_rolling_file_instance_t *self)
-{
-	if(self->fout != NULL)
-	{
-	    fclose(self->fout);
-        self->fout = NULL;
-        self->index = 0;
-	}
-}
 
 
 TERROR_CODE tlog_init(tlog_t *self, const char *config_file)
