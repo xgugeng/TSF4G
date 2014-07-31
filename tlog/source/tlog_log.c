eead41a2e0e242fdcc34ab20f2a5010ed81160a6
Guoqing Geng <xgugeng@gmail.com>
2014-07-31 14:23:45 +0800

    禁止把全局变量放入common段

--- tlog/source/tlog_log.c
+++ tlog/source/tlog_log.c
@@ -3,7 +3,8 @@
 
 #include "tlog.h"
 
-tlog_t g_tlog_instance;
+//.a里面的弱符号会在链接时出错, 编译时候使用-fno-common可以避免放入common段
+tlog_t g_tlog_instance = {};
 
 void tlog_log(tlog_t *self, tlog_level_t level, const char* file, uint32_t line, ...)
 {
